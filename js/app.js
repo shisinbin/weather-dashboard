ac9d4332147fd8210c81a29e6ee5e40b7270c90b
ssb <sb1664@gmail.com>
2022-12-21 16:12:22 +0000

    start to code logic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 // app logic goes here
+var forecastEl = $('.forecast');
+var formEl = $('form');
+
+function init() {
+  formEl.submit(function (event) {
+    event.preventDefault();
+  });
+}
+
+init();
